a137dc5c6b2fc1438b476fd1e964f62410fae358
Nancy <nyu917@gmail.com>
2025-01-24 15:39:52 +0800

    完成voteItem CRUD

--- src/main/java/com/eSun/votingSystem/controller/VoteItemsController.java
+++ src/main/java/com/eSun/votingSystem/controller/VoteItemsController.java
@@ -1,5 +1,57 @@
 package com.eSun.votingSystem.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.eSun.votingSystem.dao.VoteItemsDao;
+import com.eSun.votingSystem.service.VoteItemsService;
+
+@RestController
 public class VoteItemsController {
 
+	@Autowired
+	private VoteItemsService itemService;
+	
+	@PostMapping("/voteItem")
+	public VoteItemsDao addVoteItem(@RequestBody VoteItemsDao itemInfo) {
+		return itemService.saveItem(itemInfo);
+	}
+	
+	@GetMapping("/voteItem/{itemId}")
+	public VoteItemsDao showVoteItem(@PathVariable("itemId") Integer id) {
+		return itemService.findById(id);
+	}
+	
+	@GetMapping("/voteItem")
+	public List<VoteItemsDao> showAllVoteItem() {
+		return itemService.findAll();
+	}
+	
+	@PutMapping("/voteItem")
+	public String editItem(@RequestBody VoteItemsDao item) {
+		boolean updateItem = itemService.updateItemDetail(item);
+		if(updateItem) {
+			return "update success";
+		}
+		return "update failed";
+	}
+	
+	@DeleteMapping("/voteItem/{itemId}")
+	public String deleteItem(@PathVariable("itemId") Integer id) {
+		VoteItemsDao itemsDao = itemService.findById(id);
+		
+		if(itemsDao != null) {
+			itemService.deleteItemById(id);
+			return "delete success";
+		}
+		return "delete failed";
+	}
 }
--- src/main/java/com/eSun/votingSystem/service/VoteItemsService.java
+++ src/main/java/com/eSun/votingSystem/service/VoteItemsService.java
@@ -1,8 +1,13 @@
 package com.eSun.votingSystem.service;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
 
+import com.eSun.votingSystem.dao.VoteItemsDao;
 import com.eSun.votingSystem.dao.VoteItemsRepository;
 
 import jakarta.transaction.Transactional;
@@ -14,8 +19,41 @@ public class VoteItemsService {
 	@Autowired
 	private VoteItemsRepository itemsRepo;
 	
+	public VoteItemsDao saveItem(VoteItemsDao item) {
+		
+		return itemsRepo.save(item);
+	}
 	
+	public VoteItemsDao findById(Integer id) {
+		Optional<VoteItemsDao> optional = itemsRepo.findById(id);
+		
+		if(optional.isPresent()) {
+			VoteItemsDao itemDao = optional.get();
+			return itemDao;
+		}
+		return null;
+	}
 	
+	public List<VoteItemsDao> findAll() {
+		return itemsRepo.findAll();
+	}
 	
+	public boolean updateItemDetail(VoteItemsDao item) {
+		
+		Optional<VoteItemsDao> optional = itemsRepo.findById(item.getId());
+		
+		if(optional.isPresent()) {
+			VoteItemsDao itemFromDB = optional.get();
+			itemFromDB.setItemName(item.getItemName());
+			itemsRepo.save(itemFromDB);
+			return true;
+		}
+		
+		return false;
+	}
+	
+	public void deleteItemById(Integer id) {
+		itemsRepo.deleteById(id);
+	}
 	
 }
